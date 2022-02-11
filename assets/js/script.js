4bb205ad16b721c49b49d8e8334f838d31c69835
simlusion <98736600+simlusion@users.noreply.github.com>
2022-02-11 19:33:16 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+// Logo image hover substitution
+function hoverLogo(element) {
+  element.setAttribute('src', 'assets/img/logo2.png')
+}
+
+function unhoverLogo(element) {
+  element.setAttribute('src', 'assets/img/logo.png')
+}
+
 // Hamburger menu
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.nav-menu')
@@ -14,3 +23,20 @@ function scrollUp() {
   else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  distance: '80px',
+  duration: 2000,
+  delay: 400,
+  reset: true,
+})
+
+sr.reveal(`.sr-down-fast`, { delay: 200, origin: 'bottom' })
+sr.reveal(`.sr-down-slow`, { delay: 400, origin: 'bottom' })
+sr.reveal(`.sr-top-fast`, { delay: 200, origin: 'bottom' })
+sr.reveal(`.sr-top-slow`, { delay: 400, origin: 'bottom' })
+sr.reveal(`.sr-left-fast`, { delay: 200, origin: 'bottom' })
+sr.reveal(`.sr-left-slow`, { delay: 600, origin: 'bottom' })
+sr.reveal(`.sr-right-fast`, { delay: 200, origin: 'bottom' })
+sr.reveal(`.sr-right-slow`, { delay: 600, origin: 'bottom' })
